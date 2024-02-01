cc5a03d4111b2ae17ddea074432d933afb4028db
unknown <lchin10@bu.edu>
2024-02-01 11:29:08 -0500

    added Game page file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './App.css';
+import GamePage from './pages/Game';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const navigate = useNavigate();
+
+    // const handleEasyClick = () => {
+    //     navigate('/game');
+    // };
+    return (
+        <Router>
+            <Routes>
+                <Route path="/game" element={<GamePage />} />
+            </Routes>
+            <div className="App">
+                <header className="App-header">
+                    <div id="overlay" onClick={overlayOff} >
+                        <div>Difficulty:</div>
+                        <div id="difficulty-box" >
+                            {/* <p>Easy1</p> */}
+                            <Link to="/game" id="easy" className="column">Easy2</Link>
+                            <Link to="/game" id="medium" className="column">Medium</Link>
+                            <Link to="/game" id="hard" className="column">Hard</Link>
+                            {/* <p id="medium" className="column" >Medium</p>
+                            <p id="hard" className="column" >Hard</p> */}
+                        </div>
+                    </div>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 id="page-title" >
+                        GEOGUESSR
+                    </h1>
+                    <button onClick={startButton} id="start-button" >START</button>
+                </header>
+            </div>
+        </Router>
+    );
+}
+
+function startButton() {
+    overlayOn();
+}
+
+function overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff() {
+    document.getElementById("overlay").style.display = "none";
 }
 
 export default App;
---
+++ src/pages/Game.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 id="page-title" >
+                    GEOGUESSR
+                </h1>
+                <p>wtfffff</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
